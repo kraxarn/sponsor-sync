a7858ccbe9c84759625eb35de7d89b732aaa1fdc
kraxarn <kraxie@protonmail.com>
2021-12-27 12:18:09 +0100

    Increase length for id and video id

--- sql/00_sponsor_time/up.sql
+++ sql/00_sponsor_time/up.sql
@@ -1,7 +1,7 @@
 create table if not exists sponsor_time
 (
-    id         varchar(64) primary key,
-    video_id   varchar(14) not null,
-    start_time float    not null,
-    end_time   float    not null
+    id         varchar(96) primary key,
+    video_id   varchar(16) not null,
+    start_time float       not null,
+    end_time   float       not null
 )
